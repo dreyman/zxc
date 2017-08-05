01b217a11f2b66e6cc4ecfe58f385436099001fc
Igor <k4k7005@gmail.com>
2017-08-05 04:04:37 +0300

    simplify main.js

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
-window.onload = function() {
+'use strict';
+(function() {
+
   setProgress();
-}
 
-let getInfo = function() {
-  let date = new Date();
-  let year = date.getFullYear();
-  let isLeap = new Date(year, 1, 29).getMonth() == 1;
-  let mdays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-  if (isLeap) mdays[1] = 29;
-  let nDays = date.getDate();
-  for (let i = 0; i < date.getMonth(); nDays += mdays[i++]);
-  return {
-    year: year,
-    day: nDays,
-    daysInYear: isLeap ? 366 : 365
+  function getInfo() {
+    let date = new Date();
+    let year = date.getFullYear();
+    let isLeap = year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
+    let mdays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    if (isLeap) mdays[1] = 29;
+    let nDays = date.getDate();
+    for (let i = 0; i < date.getMonth(); nDays += mdays[i++]);
+    return {
+      year: year,
+      day: nDays,
+      daysInYear: isLeap ? 366 : 365
+    }
   }
-}
 
-function setProgress() {
-  let info = getInfo();
-  document.getElementById('i').style.width = info.day / info.daysInYear * 100 + '%';
-  document.getElementById('day').innerHTML = /* info.year + '.' + */ info.day;
-}
+  function setProgress() {
+    let info = getInfo();
+    document.getElementById('i').style.width = info.day / info.daysInYear * 100 + '%';
+    document.getElementById('day').innerHTML = /* info.year + '.' + */ info.day;
+  }
+
+})();
