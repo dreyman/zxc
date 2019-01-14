205efc24e99119eed10aef4a34a52f56e16f461a
Igor Lychak <igor@qooling.com>
2019-01-14 03:33:03 +0200

    logo stuff

--- js/about.d98caee9.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(e,t,n){"use strict";n.r(t);var a=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},s=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("save and manage personal stuff")])])}],u=n("2877"),l={},o=Object(u["a"])(l,a,s,!1,null,null,null);o.options.__file="About.vue";t["default"]=o.exports}}]);
-//# sourceMappingURL=about.d98caee9.js.map
